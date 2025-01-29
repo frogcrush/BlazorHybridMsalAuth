1edf812172e87a0fb1112e4851f7350f1679a532
Tylor Pater <tylor@paradigmpanels.com>
2025-01-29 14:57:28 -0700

    Add required platform code for iOS and Android

---
+++ BlazorHybridMsalAuth/Platforms/Android/MsalActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Content;
+using Microsoft.Identity.Client;
+
+namespace BlazorHybridMsalAuth.Platforms.Android
+{
+    [Activity(Exported = true)]
+    [IntentFilter(new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
+        DataHost = "auth",
+        DataScheme = "msal{MSAL_Application_Key}")]
+    public class MsalActivity : BrowserTabActivity
+    {
+    }
+}
--- BlazorHybridMsalAuth/Platforms/iOS/AppDelegate.cs
+++ BlazorHybridMsalAuth/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,6 @@
 ﻿using Foundation;
+using Microsoft.Identity.Client;
+using UIKit;
 
 namespace BlazorHybridMsalAuth
 {
@@ -6,5 +8,22 @@ namespace BlazorHybridMsalAuth
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, NSDictionary options)
+        {
+            if (AuthenticationContinuationHelper.IsBrokerResponse(null))
+            {
+                // Done on different thread to allow return in no time.
+                _ = Task.Factory.StartNew(() => AuthenticationContinuationHelper.SetBrokerContinuationEventArgs(url));
+
+                return true;
+            }
+            else if (!AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(url))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
